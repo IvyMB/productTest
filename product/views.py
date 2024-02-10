5148c5b38443711be73a317d551eb4c36b74e685
Iveny Milena B S <vyhofc@gmail.com>
2024-02-09 23:27:03 -0300

    Update views.py
    fix: improving code readability

--- product/views.py
+++ product/views.py
@@ -9,54 +9,34 @@ from product.serializers import ProductSerializer
 logger = logging.getLogger(__name__)
 
 
-class ProductCreateListView(generics.ListCreateAPIView):
-    # Only get and options methods is allowed without authentication token
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
+class ListCreateBaseView(generics.ListCreateAPIView):
     def create(self, request, *args, **kwargs):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            product = serializer.save()
-            logger.info(f'User {request.user.username} create a new product with ID: {product.id}.')
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        response = super().create(request, *args, **kwargs)
+        created_object_id = response.data.get('id', None)
+        if created_object_id:
+            logger.info(f'{request.user.username} created a new object with ID: {created_object_id}')
 
-        logger.info(f'User {request.user.username} has an bad request {serializer.errors}.')
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    # Only get and options methods is allowed without authentication token
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
+class RetrieveUpdateDestroyBaseView(generics.RetrieveUpdateDestroyAPIView):
     def put(self, request, *args, **kwargs):
-        identification = kwargs.get('pk', None)
-        product = self.get_object()
-
-        if product:
-            serializer = ProductSerializer(product, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-
-                # Exemplo de logging
-                logger.info(f'User {request.user.username} update the product {identification}.')
-                return Response(serializer.data, status=status.HTTP_200_OK)
-
-            logger.info(f'User {request.user.username} has an bad request {serializer.errors}.')
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        object_id = kwargs.get('pk', None)
+        if object_id:
+            logger.info(f'{request.user.username} updated a object with ID: {object_id}')
 
     def delete(self, request, *args, **kwargs):
-        identification = kwargs.get('pk', None)
-        product = self.get_object()
+        object_id = kwargs.get('pk', None)
+        if object_id:
+            logger.info(f'{request.user.username} deleted a object with ID: {object_id}')
 
-        if product:
-            product.delete()
-            logger.info(f'User {request.user.username} delete the product {identification}.')
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+
+class ProductCreateListView(ListCreateBaseView):
+    # Only get and options methods is allowed without authentication token
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductRetrieveUpdateDestroyView(RetrieveUpdateDestroyBaseView):
+    # Only get and options methods is allowed without authentication token
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
